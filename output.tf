b187cc9b792fd9562f9f6a4528bb4ea860438bf4
mynewproject123 <167633386+mynewproject123@users.noreply.github.com>
2025-09-01 23:05:47 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,16 +1,19 @@
 output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = aws_vpc.vpc_ansys.id
+  value = module.vpc.vpc_id
 }
 
 output "public_subnet_ids" {
-  description = "List of public subnet IDs"
-  value       = [aws_subnet.public_subnet1.id, aws_subnet.public_subnet2.id, aws_subnet.public_subnet3.id ]
+  value = module.vpc.public_subnet_ids
 }
 
 output "private_subnet_ids" {
-  description = "Private subnet IDs"
-  value       = [aws_subnet.private_subnet1.id, aws_subnet.private_subnet2.id, aws_subnet.private_subnet3.id ]
+  value = module.vpc.private_subnet_ids
 }
 
+output "instance_id" {
+  value = module.ec2.instance_id
+}
 
+output "public_ip" {
+  value = module.ec2.public_ip
+}
