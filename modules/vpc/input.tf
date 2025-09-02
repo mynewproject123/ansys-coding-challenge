e6742446aa0ed2aaf57a33d5453e5a5ab0ffe4d2
mynewproject123 <167633386+mynewproject123@users.noreply.github.com>
2025-09-01 23:06:49 -0400

    Update input.tf

--- modules/vpc/input.tf
+++ modules/vpc/input.tf
@@ -1,24 +1,15 @@
-variable "aws_region" {
-  description = "AWS region to deploy resources"
-  type        = string
+variable "vpc_cidr_block" {
+  type = string
 }
 
-variable "vpc_cidr_block" {
-  description = "CIDR block for the VPC"
-  type        = string
-  }
-
 variable "availability_zones" {
-  description = "List of availability zones"
-  type        = list(string)
-  }
+  type = list(string)
+}
 
 variable "public_subnet_cidrs" {
-  description = "List of CIDR blocks for public subnets"
-  type        = list(string)
-  }
+  type = list(string)
+}
 
 variable "private_subnet_cidrs" {
-  description = "List of CIDR blocks for private subnets"
-  type        = list(string)
-  }
+  type = list(string)
+}
